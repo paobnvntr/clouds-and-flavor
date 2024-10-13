297f7cc6ad1318c9013131a78cd88db14031a85c
Paolo Buenaventura <paolo.buenaventurag240404@gmail.com>
2024-10-13 14:45:32 +0800

    update: fixed design and contents of footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,57 +1,50 @@
-<!-- Footer Section Begin -->
 <footer class="footer spad">
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-3 col-md-6 col-sm-6">
-          <div class="footer__about">
-            <div class="footer__about__logo">
-              <a href="dashboard"><img src="{{asset('assets/img/CNF.jpg')}}" alt="" /></a>
-            </div>
-            <ul>
-              <li>Address: Address Sample blk 2</li>
-              <li>Phone: +63 </li>
-              <li>Email: example@domain.com</li>
-            </ul>
-          </div>
-        </div>
-        <div class="col-lg-4 col-md-6 col-sm-6 offset-lg-1">
-          <div class="footer__widget">
-            <h6>Useful Links</h6>
-            <ul>
-              <li><a href="{{url('dashboard')}}">Home</a></li>
-              <li><a href="{{url('/products')}}">Shop</a></li>
-              <li><a href="{{url('/my-cart')}}">Cart</a></li>
-              <li><a href="{{url('/my-order')}}">Order</a></li>
-            </ul>
-        
-          </div>
-        </div>
-        <div class="col-lg-4 col-md-12">
-          <div class="footer__widget">
-            <div class="footer__widget__social">
-              <a href="#"><i class="fa fa-facebook"></i></a>
-              <a href="#"><i class="fa fa-instagram"></i></a>
+	<div class="container">
+		<div class="row">
+			<div class="col-6">
+				<div class="footer__about">
+					<div class="footer__about__logo" style="width: 50%;">
+						<img src="{{asset('assets/img/cnfhomepage.png')}}"/>
+					</div>
+					<ul>
+						<li><strong>Address: </strong><span class="text-muted">Block 23 Acacia Cor Tanguile Street, Calendola San Pedro, Laguna</span></li>
+						<li><strong>Phone: </strong><span class="text-muted">0906 587 2891</span></li>
+						<li><strong>Email Address: </strong><span class="text-muted">cloudsnflavor@gmail.com</span></li>
+					</ul>
+				</div>
+			</div>
+			<div class="col-4">
+				<div class="footer__widget">
+					<h6>Useful Links</h6>
+					<ul>
+						<li><a href="{{url('dashboard')}}">Home</a></li>
+						<li><a href="{{url('/products')}}">Shop</a></li>
+						<li><a href="{{url('/my-cart')}}">Cart</a></li>
+						<li><a href="{{url('/my-order')}}">Order</a></li>
+					</ul>
 
-          </div>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-lg-12">
-          <div class="footer__copyright">
-            <div class="footer__copyright__text">
-              <p>
-                Copyright &copy;
-                <script>
-                  document.write(new Date().getFullYear());
-                </script>
-                All rights reserved | 
-                <a href="#" target="_blank">cnfvape</a>
-              </p>
-            </div>
-            
-          </div>
-        </div>
-      </div>
-    </div>
-  </footer>
-  <!-- Footer Section End -->
+				</div>
+			</div>
+			<div class="col-2">
+				<div class="footer__widget">
+					<div class="footer__widget__social">
+						<a href="https://www.facebook.com/profile.php?id=100078434664934" target="_blank"><i
+								class="fa fa-facebook"></i></a>
+					</div>
+				</div>
+			</div>
+			<div class="col-12">
+				<div class="footer__copyright d-flex justify-content-center">
+					<div class="footer__copyright__text">
+						<p>
+							Copyright &copy;
+							<script>
+								document.write(new Date().getFullYear());
+							</script>
+							All rights reserved | Clouds N Flavor
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+</footer>
