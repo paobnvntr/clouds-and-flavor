21068e6a4c5fe5b72be6adb3f1aa6430606ba0b0
Paolo Buenaventura <paolo.buenaventurag240404@gmail.com>
2024-10-13 23:41:16 +0800

    Update: Login and Register Modifications

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,7 +5,9 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>@yield('title')</title>
+
+        <link rel="shortcut icon" href="{{ asset('assets/img/CNF.jpg') }}" type="image/x-icon">
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
